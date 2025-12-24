332d4bf72b0b7ba06bd2e480c664d2b6c2f23941
Ashish-web-tech <akonwtf8@gmail.com>
2025-12-24 21:25:09 +0530

    client env added

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -29,7 +29,7 @@ const Input = ({ refresh, setRefresh }) => {
     setLoading(true);
 
     try {
-      const response = await axios.post("http://localhost:3000/api/scrape", {
+      const response = await axios.post(import.meta.env.VITE_API_URL, {
         url,
         isTrack,
       });
